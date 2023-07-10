db31633411305db4346c78e6e6a6e1ff086a644f
Tonysoossa <tony.servanin@gmail.com>
2023-07-10 16:52:00 +0200

    Router partly set

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Root } from "./routes/Root.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorPage } from "./ErrorPage.tsx";
+import { Contact } from "./routes/Contact.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
